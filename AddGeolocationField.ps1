954ff334a89daecaa8a47bca589f085cee74e3e0
PowershellScripts <42035526+PowershellScripts@users.noreply.github.com>
2018-08-02 17:39:58 +0300

    Update AddGeolocationField.ps1
    minor changes and link to Microsoft Docs for more options

--- AddGeolocationField.ps1
+++ AddGeolocationField.ps1
@@ -9,25 +9,28 @@ param (
 		$Password,
         [Parameter(Mandatory=$true,Position=4)]
 		[string]$ListTitle
-
 		)
 
-
+# Create client context
   $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($url)
   $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
   $web=$ctx.Web
   $ctx.Load($web)
   $ctx.ExecuteQuery()
 
+#Load the list to add the column to
   $list = $web.Lists.GetByTitle($ListTitle)
   $ctx.Load($list)
   $ctx.ExecuteQuery()
 
+#Add the geolocation field
   $geolocationfield=$list.Fields.AddFieldAsXml("<Field Type='Geolocation' DisplayName='Location'/>", $true, [Microsoft.SharePoint.Client.AddFieldOptions]::AddToAllContentTypes)
   $list.Update()
   $ctx.ExecuteQuery()
 
-  <#
+  <# Additional code for creating a map view at the same time. Creating a map view can be achieved via UI. 
+  You can follow steps described here:https://docs.microsoft.com/en-us/sharepoint/dev/general-development/create-a-map-view-for-the-geolocation-field-in-sharepoint 
+   
   $ViewCreationInfo = New-Object Microsoft.SharePoint.Client.ViewCreationInformation
   $ViewCreationInfo.Title="MapView"
   $ViewCreationInfo.ViewFields=("Title","Location2")
@@ -37,16 +40,11 @@ param (
   #$View.DefaultView=$true
   $View.Update()
   $ctx.ExecuteQuery()
-  $View.JSLink
 
-  
-  $ctx.Web.AllProperties["BING_MAPS_KEY"]="AjtUzWJBHlI3Ma_Ke6Qv2fGRXEs0ua5hUQi54ECwfXTiWsitll4AkETZDihjcfeI"
+  $ctx.Web.AllProperties["BING_MAPS_KEY"]="AjtUzWJBHlI3Ma_Ke6blaa5hUQi54ECwNOTRealll4AkETZDihjcfeI"
   $ctx.Web.Update()
-  $ctx.ExecuteQuery()
-    
+  $ctx.ExecuteQuery()   
      #>
-
- 
 
 }
 
